3a05d68a9ba2bbc0d1cab8b5dea3ba4f71a37eac
JackVacek <vacekjack@gmail.com>
2025-03-11 22:14:20 -0500

    littlemore

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 I am working through the CP4 Book 1 right now and I am solving the starred Kattis questions as I go through it.
 I am currently on part 1.6 (page 36 on the paperback and page 66 on the pdf)
-I am going to skip to chapter 4 and 8.4-8.5 for my class.
+I am going to skip to chapter 4 (after I finish chapter 1) and 8.4-8.5 for my class.
--- chapter1/problems/memoryMatch.py
+++ chapter1/problems/memoryMatch.py
@@ -1,1 +1,29 @@
-#this is the next problem to do
+N = int(input())
+K = int(input())
+cards = ["Unknown"] * N
+for i in range(K):
+    parts = input().split()
+    c1,c2=int(parts[0])-1, int(parts[1])-1
+    p1,p2=parts[2],parts[3]
+    cards[c1]=p1
+    cards[c2]=p2
+    if p1 == p2:
+        cards[c1] = 'Matched'
+        cards[c2] = 'Matched'
+faceDown = [card for card in cards if card != 'Matched']
+picCounts = {}
+for card in faceDown:
+    if card != 'Unknown':
+        picCounts[card] = picCounts.get(card,0) + 1
+pairs = 0
+singles = 0
+for pic, count in picCounts.items():
+    pairs += count // 2
+    singles += count % 2
+unknown = faceDown.count("Unknown")
+score = pairs
+if singles >= unknown:
+    score += unknown
+elif unknown == 2:
+    score += 1
+print(score)
